a81ebf1d0bf540ebe421056e1f0a9213316aa4dc
Brendan Hellar and Mariia Stashuk <mariia@>
2019-03-25 10:13:08 -0700

    convert cups, gallons, tablespoons

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var cup = function(number1) {
+  return number1 * 8;
+
+};
+
+var number1 = parseInt(prompt("Enter cups"));
+
+alert (cup(number1));
+
+
+var gallon = function(number2) {
+  return number2 *  3.785;
+
+};
+
+var number2 = parseInt(prompt("Enter gallons"));
+
+alert (gallon(number2));
+
+var gallon = function(number3) {
+  return number3 *  14.787;
+
+};
+
+var number3 = parseInt(prompt("Enter tablespoons"));
+
+alert (gallon(number3));
